a548ca97e322ad7859a000e25022486b724b6eb3
Silabear <56885288+Silabear@users.noreply.github.com>
2025-05-24 21:53:32 +0100

    meta stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Minecraft Text Editor by Datapack Hub</title>
+</svelte:head>
+
 <div class="font-lexend bg-zinc-900 text-white min-h-screen h-full w-full">
 	{@render children()}
 </div>

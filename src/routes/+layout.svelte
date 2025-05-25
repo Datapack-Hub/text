f6a0a9b1c73be32fc6411a1fe437649046c86f5e
Silabear <56885288+Silabear@users.noreply.github.com>
2025-05-25 19:21:06 +0100

    meta changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,14 @@
 </script>
 
 <svelte:head>
-	<title>Minecraft Text Editor by Datapack Hub</title>
+	<title>Minecraft Text Editor</title>
+	<meta name="title" content="Minecraft Text Editor">
+	<meta name="description" content="Create text components (JSON text) for Minecraft Java Edition commands with our UI!">
+	<meta name="keywords" content="datapacks, commands, minecraft, text, json text, minecraft tools">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta name="language" content="English">
+	<meta name="author" content="Datapack Hub">
 </svelte:head>
 
 <div class="font-lexend bg-zinc-900 text-white min-h-screen h-full w-full">

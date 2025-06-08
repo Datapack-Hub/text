eed400b17a554756c7db0d3e5c961aad15c512bb
Silabear <56885288+Silabear@users.noreply.github.com>
2025-06-08 21:17:39 +0100

    quick fix

--- src/lib/tiptap/text.ts
+++ src/lib/tiptap/text.ts
@@ -438,9 +438,10 @@ export function optimise(arr: StringyMCText[], lore = false): StringyMCText[] {
 	// 3: Remove leading empty string if followed by a string
 	if (out.length >= 2 && out[0] === "" && typeof out[1] === "string") out.shift();
 
-	// 4: If out[1] is a string, or an object without any style properties, then remove out[0]
+	// 4: If out[0] is a blank string, out[1] is a string, or an object without any style properties, then remove out[0]
 	if (
 		out.length >= 2 &&
+		out[0] == "" &&
 		(
 			typeof out[1] === "string" ||
 			(

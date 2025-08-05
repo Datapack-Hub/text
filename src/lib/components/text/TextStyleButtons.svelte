fd4cf35ae2873d1a7287a577112a8f1c7d6cb138
boop-the-dog <aidenhperry@gmail.com>
2025-08-05 13:30:21 -0400

    add the files

---
+++ src/lib/components/text/TextStyleButtons.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	import { tooltip } from "$lib/tooltip";
+	import IconBold from "~icons/tabler/bold";
+	import IconItalic from "~icons/tabler/italic";
+	import IconObfuscate from "~icons/tabler/password";
+	import IconShadow from "~icons/tabler/shadow";
+	import IconStrikethrough from "~icons/tabler/strikethrough";
+	import IconUnderline from "~icons/tabler/underline";
+
+	const { editor, small = false } = $props();
+	let shadowColorValue = $state("#ffffff");
+</script>
+
+<button
+	aria-label="Bold"
+	onclick={() => editor.chain().focus().toggleBold().run()}
+	{@attach tooltip}
+	class="p-1 {small
+		? 'text-sm'
+		: 'text-lg'} rounded-md font-medium hover:bg-white/3 {editor.isActive(
+		'bold',
+	)
+		? 'bg-zinc-800'
+		: ''}">
+	<IconBold />
+</button>
+<button
+	aria-label="Italic"
+	onclick={() => editor.chain().focus().toggleItalic().run()}
+	{@attach tooltip}
+	class="p-1 {small
+		? 'text-sm'
+		: 'text-lg'} rounded-md font-medium hover:bg-white/3 {editor.isActive(
+		'italic',
+	)
+		? 'bg-zinc-800'
+		: ''}">
+	<IconItalic />
+</button>
+<button
+	aria-label="Strikethrough"
+	onclick={() => editor.chain().focus().toggleStrike().run()}
+	{@attach tooltip}
+	class="p-1 {small
+		? 'text-sm'
+		: 'text-lg'} rounded-md font-medium hover:bg-white/3 {editor.isActive(
+		'strike',
+	)
+		? 'bg-zinc-800'
+		: ''}">
+	<IconStrikethrough />
+</button>
+<button
+	aria-label="Underline"
+	onclick={() => editor.chain().focus().toggleUnderline().run()}
+	{@attach tooltip}
+	class="p-1 {small
+		? 'text-sm'
+		: 'text-lg'} rounded-md font-medium hover:bg-white/3 {editor.isActive(
+		'underline',
+	)
+		? 'bg-zinc-800'
+		: ''}">
+	<IconUnderline />
+</button>
+<button
+	aria-label="Obfuscated"
+	onclick={() => editor.chain().focus().toggleObfuscated().run()}
+	{@attach tooltip}
+	class="p-1 {small
+		? 'text-sm'
+		: 'text-lg'} rounded-md font-medium hover:bg-white/3 {editor.isActive(
+		'obfuscated',
+	)
+		? 'bg-zinc-800'
+		: ''}">
+	<IconObfuscate />
+</button>
+{#if editor.isActive("shadowColor")}
+	<button
+		aria-label="Shadow Color"
+		name="shadow_color"
+		{@attach tooltip}
+		class="p-1 {small
+			? 'text-sm'
+			: 'text-lg'} rounded-md bg-zinc-800 font-medium hover:bg-white/3"
+		onclick={() => editor.chain().focus().unsetShadowColor().run()}>
+		<IconShadow />
+	</button>
+{:else}
+	<label
+		for="shadow_color"
+		aria-label="Shadow Color"
+		{@attach tooltip}
+		class="p-1 {small
+			? 'text-sm'
+			: 'text-lg'} rounded-md font-medium hover:bg-white/3">
+		<IconShadow />
+	</label>
+	<input
+		type="color"
+		id="shadow_color"
+		class="w-0"
+		bind:value={shadowColorValue}
+		onchange={() =>
+			editor.chain().focus().setShadowColor(shadowColorValue).run()} />
+{/if}
---
+++ src/lib/tiptap/extensions/nodes/ObjectNode.ts
@@ -0,0 +1,66 @@
+import { Node, mergeAttributes, type CommandProps } from "@tiptap/core";
+import type { NodeOptions, ObjectAttributes } from "../index";
+
+export const ObjectNode = Node.create<NodeOptions>({
+	name: "object",
+
+	inline: true,
+	group: "inline",
+	atom: true,
+
+	addOptions() {
+		return {
+			HTMLAttributes: {},
+		};
+	},
+
+	addAttributes(): ObjectAttributes {
+		return {
+			atlas: "",
+			sprite: "",
+		};
+	},
+
+	parseHTML() {
+		return [
+			{
+				tag: "span[data-object-node]",
+			},
+		];
+	},
+
+	renderHTML({ HTMLAttributes, node }) {
+		const { atlas, sprite } = node.attrs;
+
+		return [
+			"span",
+			mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
+				"data-object-node": "true",
+				contenteditable: "false",
+				style: `
+            background-color: #18181b;
+            padding: 0px 5px;
+            border-radius: 4px;
+            font-size: 0.9rem;
+            display: inline-block;
+			vertical-align: var(--custom-source-align, middle);
+			text-decoration: inherit;
+        `,
+			}),
+			["span", {}, `OBJECT: ${atlas}/${sprite}`],
+		];
+	},
+
+	addCommands() {
+		return {
+			insertObject:
+				(attrs) =>
+				({ commands }: CommandProps) => {
+					return commands.insertContent({
+						type: this.name,
+						attrs,
+					});
+				},
+		};
+	},
+});

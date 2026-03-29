aa6b9ba29fe81ec9f3f2f82ece7e884f50004907
Cobblestone <aidenhperry@gmail.com>
2026-03-29 11:05:18 -0400

    compliance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -61,6 +61,7 @@
 	import { fontLUT } from "$lib/tiptap/extensions/fonts";
 	import { tooltip } from "$lib/tooltip";
 	import { versions, type Version } from "$lib/types";
+	import FaceScan from "$lib/components/FaceScan.svelte";
 
 	let tiptapJSON: JSONContent = $state()!;
 
@@ -391,6 +392,7 @@
 <svelte:window onkeydown={clearMarksHandler} />
 
 <main class="flex h-screen max-h-screen flex-col">
+	<FaceScan />
 	<div
 		class="flex w-full items-center bg-zinc-950 text-zinc-300"
 		style="font-family: Lexend">

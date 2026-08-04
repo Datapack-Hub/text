a2c641a87e8e008229db81fe70fd81808eea04dc
Cobblestone <aidenhperry@gmail.com>
2026-08-04 11:36:55 -0400

    fix problem

--- vite.config.ts
+++ vite.config.ts
@@ -10,18 +10,6 @@ export default defineConfig({
         projects: [
             {
                 extends: "./vite.config.ts",
-                plugins: [svelteTesting()],
-                test: {
-                    name: "client",
-                    environment: "jsdom",
-                    clearMocks: true,
-                    include: ["src/tests/unit/**/*.svelte.{test,spec}.{js,ts}"],
-                    exclude: ["src/lib/server/**"],
-                    setupFiles: ["./vitest-setup-client.ts"],
-                },
-            },
-            {
-                extends: "./vite.config.ts",
                 test: {
                     name: "server",
                     environment: "node",

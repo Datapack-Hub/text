7f24c794b41bc1f4229f6f03912d7ab1f94f1bb7
Silabear <56885288+Silabear@users.noreply.github.com>
2026-09-01 01:23:52 +0100

    improve the custom source modal while im at it

--- src/lib/tooltip.ts
+++ src/lib/tooltip.ts
@@ -13,3 +13,17 @@ export function tooltip(e: HTMLElement) {
         tip.destroy();
     };
 }
+
+export function tooltip_right(e: HTMLElement) {
+    const tip = tippy(e, {
+        placement: "right",
+        animation: "shift-away",
+        arrow: true,
+        theme: "dph",
+        content: e.ariaLabel || e.title || e.textContent || "",
+    });
+
+    return () => {
+        tip.destroy();
+    };
+}
